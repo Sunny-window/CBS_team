dafd7868ecdaa96283a801aee37ea695ecca3060
KwonHyun <rnjsgus21@naver.com>
2024-05-03 10:04:19 +0900

    로그아웃 인데스 오류 ^^

--- cbs/src/main/java/com/green/cbs/controller/UserController.java
+++ cbs/src/main/java/com/green/cbs/controller/UserController.java
@@ -83,7 +83,7 @@ public class UserController {
          
     	session.invalidate();
     	
-    	return "redirect:/index";
+    	return "redirect:/";
     }
     
     @RequestMapping("/loginForm")
